0c4a81cfb4b22bf5f618c667e641e4b6a5d9efff
iamfortune <ikechifortune@gmail.com>
2020-04-23 16:20:26 +0100

    fixed bug with navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import AppNavbar from './components/layout/AppNavbar';
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <AppNavbar />
       <h1>Hello</h1>
     </div>
+    </Router>
   );
 }
 
