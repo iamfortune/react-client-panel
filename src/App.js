b9f2867b5c3507d13ddb39dbe892a5e430d0e6bc
iamfortune <ikechifortune@gmail.com>
2020-04-23 16:36:27 +0100

    added dashboard files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import AppNavbar from './components/layout/AppNavbar';
+import Dashboard from './components/layout/Dashboard';
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <AppNavbar />
-      <h1>Hello</h1>
-    </div>
+      <div className="App">
+        <AppNavbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+          </Switch>
+        </div>  
+      </div>
     </Router>
   );
 }
---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Clients from '../clients/Clients';
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    )
+}
+
+
+export default Dashboard;
