0d8ae99fc975c436681440847bd8f68bf22ac3dc
iamfortune <ikechifortune@gmail.com>
2020-04-24 10:07:50 +0100

    added sidebar code

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+
+export default function Sidebar() {
+    return (
+        <Link to="/client/add" className="btn btn-success">
+        </Link>
+    )
+}
